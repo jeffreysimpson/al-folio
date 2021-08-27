86db35ccfcc1acc4d73b330f5ddb652ae1f7098f
Jeffrey Simpson <jeffrey.simpson@unsw.edu.au>
2021-08-27 21:16:01 +1000

    Add script for creating the .bib files.
    Some minor formatting changes as well.

---
+++ _bibliography/create_papersbib.py
@@ -0,0 +1,152 @@
+# encode: utf-8
+"""
+Convert your ADS Libraries into the markdown publication pages.
+"""
+
+import os 
+import requests
+import pandas as pd
+import numpy as np
+
+def get_config():
+    """
+    Load ADS developer key from file
+    :return: str
+    """
+#     token = os.getenv('ADS_TOKEN')
+    try:
+        with open(os.path.expanduser('~/.ads/dev_key')) as f:
+            token = f.read().strip()
+    except IOError:
+        print('The script assumes you have your ADS developer token in the'
+              'folder: {}'.format())
+
+    return {
+        'url': 'https://api.adsabs.harvard.edu/v1/biblib',
+        'headers': {
+            'Authorization': 'Bearer:{}'.format(token),
+            'Content-Type': 'application/json',
+        }
+    }
+
+
+def get_bibcodes(library_id):
+    start = 0
+    rows = 1000
+    config = get_config()
+
+    r = requests.get(
+        '{}/libraries/{id}?start={start}&rows={rows}'.format(
+            config['url'],
+            id=library_id,
+            start=start,
+            rows=rows
+        ),
+        headers=config['headers']
+    )
+    # Get all the documents that are inside the library
+    try:
+        bibcodes = r.json()['documents']
+    except ValueError:
+        raise ValueError(r.text)
+    return bibcodes
+
+def get_pub_df(library_id):
+    config = get_config()
+    bibcodes = get_bibcodes(library_id)
+
+    fields_wants = ["bibcode","title","year","bibstem","author_count","citation_count",
+                    "volume","pub","page_range","issue","identifier","author","doi","date","doctype",
+                    "abstract", "bibstem"]
+
+    r = requests.post("https://api.adsabs.harvard.edu/v1/search/bigquery",
+                      params={"q": "*:*",
+                              "fl": ",".join(fields_wants),
+                              "rows": 2000},
+                      headers={'Authorization': config['headers']['Authorization'],
+                               'Content-Type': 'big-query/csv'},
+                      data="bibcode\n" + "\n".join(bibcodes))
+    doc_dict = r.json()['response']['docs']
+
+    pub_df = pd.DataFrame(doc_dict)
+    pub_df.fillna(value=" ", inplace=True)
+    return pub_df
+
+def reorder(a):
+    a = a.replace("Galah", "GALAH")
+    if "," not in a:
+        return a
+    return " ".join([a.split(", ")[1],a.split(", ")[0]])
+
+def get_arxiv_str(pub):
+    arXiv_id = [i for i in pub['identifier'] if i.startswith("arXiv:")]
+    if len(arXiv_id) == 0:
+        return None
+    return f"{arXiv_id[0]}"
+
+def write_bibtex(bibtex_file, pub_df):
+    with open(bibtex_file, 'w') as bibfile:
+        for *_, publication in pub_df.sort_values(['date', 'bibcode'], ascending=[False, True]).iterrows():
+            if publication.doctype in ignore_doctype:
+                continue
+            bibfile.write(f"@{publication.doctype}")
+            bibfile.write("{")
+            bibfile.write(f"{publication.bibcode},\n")
+            bibfile.write(f"  year={{{publication.year}}},\n")
+            bibfile.write(f"  title={{{publication.title[0]}}},\n")
+            bibfile.write(f"  author={{{' and '.join([reorder(a) for a in publication.author])}}},\n")
+            bibfile.write(f"  journal={{{publication.bibstem[0]}}},\n")
+            if publication.volume != ' ':
+                bibfile.write(f"  volume={{{publication.volume}}},\n")
+            if publication.issue != ' ':
+                bibfile.write(f"  issue={{{publication.issue}}},\n")
+            if publication.page_range != ' ':
+                bibfile.write(f"  pages={{{publication.page_range}}},\n")
+            if publication.doi != ' ':
+                bibfile.write(f"  doi={{{publication.doi[0]}}},\n")
+            if get_arxiv_str(publication) is not None:
+                bibfile.write(f"  arxiv={{{get_arxiv_str(publication)}}},\n")
+            bibfile.write(f"  html={{https://ui.adsabs.harvard.edu/abs/{publication.bibcode}}},\n")
+            bibfile.write(f"  abstract={{{publication.abstract}}},\n")
+            if publication.bibcode in selected_publications:
+                bibfile.write(f"  selected={{true}},\n")
+            bibfile.write("}\n\n")
+
+selected_publications = ["2021MNRAS.507...43S",
+                         "2021MNRAS.505.5340M",
+                         "2020MNRAS.495L.129K",
+                         "2020MNRAS.491.2465K",
+                         "2020MNRAS.491.3374S",
+                         "2020Natur.583..768W",
+                         "2019MNRAS.490..741S"]
+ignore_doctype = ["catalog", "proposal", "inproceedings", "abstract"]
+            
+write_bibtex("assets/bibliography/projects.bib", get_pub_df("8YvxNAmnT1Kf09WBpxQ4Sg"))
+
+
+pub_df = get_pub_df("XWjvILPkS_qyDvkCLtLOPw")
+write_bibtex("_bibliography/papers.bib", pub_df)
+
+h_index = sum(c >= i + 1 for i, c in enumerate(sorted(pub_df.citation_count, reverse=True)))
+
+with open("_pages/publications.md", 'w') as pub_md:
+    pub_md.write(f"""---
+layout: page
+permalink: /publications/
+title: publications
+description: all of my publications in reversed chronological order. generated by jekyll-scholar.
+years: {sorted([int(y) for y in pub_df[~np.isin(pub_df.doctype,ignore_doctype)].year.unique()],reverse=True)}
+nav: true
+---
+
+<div class="publications">
+""")
+    pub_md.write(f"""Metrics are obviously problematic, but anyway, I have an h-index of {h_index}.""")
+    pub_md.write("""
+{% for y in page.years %}
+  <h2 class="year">{{y}}</h2>
+  {% bibliography -f papers -q @*[year={{y}}]* %}
+{% endfor %}
+
+</div>
+""")
